f3ae2f5ea4c28d4d7bad57f222b4708b01d85d1d
Christopher Mrakovcic <cmrakovcic@aol.com>
2020-12-22 14:44:10 -0500

    commit tree 39ebc7a040c3d0126f826421870075f9575e0ff0

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name, :deposit
 
+    def initialize(name, balance=1000)
+        @name = name
+        @balance = balance
+        @status = "open"
+    end
+
+    def deposit(deposit)
+        @balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+          true
+        else
+          false
+        end
+      end
+
+    def close_account
+        @status = "closed"
+    end
 end
